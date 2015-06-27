1f26df46cd21dcb570f8cc4cce14f5c5de63f876
brent-potter <brent.ltd@gmail.com>
2015-06-27 08:43:27 -0400

    early stage of portfolio site complete

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,11 @@ var gulp = require('gulp'),
   rename = require('gulp-rename'),
     sass = require('gulp-sass'),
     maps = require('gulp-sourcemaps'),
-     del = require('del');
+    autoprefixer = require('gulp-autoprefixer'),
+     del = require('del'),
+     livereload = require('gulp-livereload'),
+     minifyCSS = require('gulp-minify-css'),
+     rsync = require('gulp-rsync');
 
 gulp.task("concatScripts", function() {
   return gulp.src([
@@ -19,24 +23,41 @@ gulp.task("minifyScripts", ["concatScripts"], function() {
   return gulp.src("js/app.js")
   .pipe(uglify())
   .pipe(rename('app.min.js'))
-  .pipe(gulp.dest('js'));
+  .pipe(gulp.dest('js'))
+  .pipe(livereload());
 });
 
 gulp.task('sassCompile', function() {
   return gulp.src("scss/**/*.scss")
   .pipe(maps.init())
   .pipe(sass({
-    // includePaths: require('node-bourbon').with('other/path', 'another/path') 
-    // - or - 
-    includePaths: require('node-bourbon').includePaths
+    includePaths: require('node-neat').includePaths
   }))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(minifyCSS())
+  .pipe(rename('application.min.css'))
   .pipe(maps.write('./'))
-  .pipe(gulp.dest('css'));
+  .pipe(gulp.dest('css'))
+  .pipe(livereload());
 });
 
 gulp.task('watchFiles', function() {
+  livereload.listen();
   gulp.watch('scss/**/*.scss', ['sassCompile']);
   gulp.watch('js/main.js', ['concatScripts']);
+});
+
+gulp.task('deploy', ['clean', 'build'], function() {
+  return gulp.src('dist/**/*.*')
+    .pipe(rsync({
+      root: 'dist',
+      hostname: '107.170.124.191',
+      username: 'root',
+      destination: '/usr/share/nginx/html/staging'
+    }));
 });
 
 gulp.task('clean', function() {
@@ -46,7 +67,7 @@ gulp.task('clean', function() {
 gulp.task('serve', ['watchFiles']);
 
 gulp.task("build", ['minifyScripts', 'sassCompile'], function() {
-  return gulp.src(["css/application.css*", "js/app.min.js", '*.html', "img/**"], { base: './'})
+  return gulp.src(["css/application.min.css", "js/app.min.js", '*.html', "img/**"], { base: './'})
   .pipe(gulp.dest('dist'));
 });
 
